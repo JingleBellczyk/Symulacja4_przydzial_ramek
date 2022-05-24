ec7da8d0da1e1e1c7e11dcb4e0d18fa6dcc867f1
JingleBellczyk <agabela121@gmail.com>
2022-05-24 14:43:25 +0200

    commit1

---
+++ Algorithm.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Algorithm{
+    List<Process> processList;
+    List<Process> oldProcessesList;
+
+    public Algorithm(List<Process> processList) {
+        this.processList = processList;
+    }
+}
